33f2abbe543162d757c76f1bbac97066b4271aca
EduardoBran <eduardo.ads1814@gmail.com>
2025-08-01 17:24:04 -0300

    adicionando abstract classe que liga as interfaces de repositorio as implementacoes concretas para o Hilt

--- app/src/main/java/com/luizeduardobrandao/obra/di/RepositoryModule.kt
+++ app/src/main/java/com/luizeduardobrandao/obra/di/RepositoryModule.kt
@@ -1,4 +1,54 @@
 package com.luizeduardobrandao.obra.di
 
-class RepositoryModule {
+import com.luizeduardobrandao.obra.data.repository.AuthRepository
+import com.luizeduardobrandao.obra.data.repository.CronogramaRepository
+import com.luizeduardobrandao.obra.data.repository.FuncionarioRepository
+import com.luizeduardobrandao.obra.data.repository.NotaRepository
+import com.luizeduardobrandao.obra.data.repository.ObraRepository
+import com.luizeduardobrandao.obra.data.repository.impl.AuthRepositoryImpl
+import com.luizeduardobrandao.obra.data.repository.impl.CronogramaRepositoryImpl
+import com.luizeduardobrandao.obra.data.repository.impl.FuncionarioRepositoryImpl
+import com.luizeduardobrandao.obra.data.repository.impl.NotaRepositoryImpl
+import com.luizeduardobrandao.obra.data.repository.impl.ObraRepositoryImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+// Liga as interfaces de repositório às implementações concretas para o Hilt.
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindAuthRepository(
+        impl: AuthRepositoryImpl
+    ): AuthRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindObraRepository(
+        impl: ObraRepositoryImpl
+    ): ObraRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindFuncionarioRepository(
+        impl: FuncionarioRepositoryImpl
+    ): FuncionarioRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindNotaRepository(
+        impl: NotaRepositoryImpl
+    ): NotaRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindCronogramaRepository(
+        impl: CronogramaRepositoryImpl
+    ): CronogramaRepository
 }
