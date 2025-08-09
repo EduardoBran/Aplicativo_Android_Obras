8abf137ee26d1971ef02c4484e840cb3402720d8
EduardoBran <eduardo.ads1814@gmail.com>
2025-08-09 10:51:16 -0300

    implementacao para usuario logado ao fechar aplicativo entrar automaticamente em WorkFragment

--- app/src/main/java/com/luizeduardobrandao/obra/MainActivity.kt
+++ app/src/main/java/com/luizeduardobrandao/obra/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.luizeduardobrandao.obra.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import androidx.navigation.fragment.NavHostFragment
+import com.google.firebase.auth.FirebaseAuth
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -20,10 +22,26 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Edge-to-edge insets no root com id @id/main
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Define o startDestination dinamicamente (Login ou Work)
+        if (savedInstanceState == null) {
+            val navHost = supportFragmentManager
+                .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+            val navController = navHost.navController
+
+            val graph = navController.navInflater.inflate(R.navigation.main_graph)
+            val loggedIn = FirebaseAuth.getInstance().currentUser != null
+
+            graph.setStartDestination(
+                if (loggedIn) R.id.workFragment else R.id.loginFragment
+            )
+            navController.graph = graph
         }
     }
 }
